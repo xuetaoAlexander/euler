48a6d27869605827c77c4a9f4ac5e8f0145732fa
xuetaozhang <yselie06@gmail.com>
2019-08-07 00:51:09 +0800

    素数筛

--- 7.c
+++ 7.c
@@ -13,10 +13,10 @@ int prime[max_n + 5];
 
 int main() {
     memset(prime, 0, sizeof(prime));
-    for (int i = 2; i <= max_n; ++i) {
+    for (int i = 2; i < max_n; ++i) {
         if (!prime[i]) {
             prime[++prime[0]] = i;
-            for (int j = 2; j * i <= max_n; ++j) {
+            for (int j = 2; j * i < max_n; ++j) {
                 prime[j * i] = 1;
             }
         }
